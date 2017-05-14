a03398806a858d19a14f09c14f72e25033b7bc8e
rinechran <rinechran@outlook.jp>
2017-05-14 01:02:53 -0700

    main

--- operator System-1/operator System/main.cpp
+++ operator System-1/operator System/main.cpp
@@ -1,9 +1,16 @@
+#include <algorithm>
 #include "Process.h"
 #include "RR.hpp"
+#include "SJF.hpp"
 #include "Feeback.hpp"
 
 
 
+
 int main() {
+	SJF b;
+	b.run();
+	Feeback<2, 3> a;
+	a.run();
 
 }
